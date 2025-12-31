d9f0ea4ad1b8f917989ca5622cc67dbdddb539b1
nirch <nirh2@yahoo.com>
2025-12-31 08:43:50 +0200

    feat: Add examples for if/else, ternary operator, and switch statements.

--- pre-course/condtionals/index.js
+++ pre-course/condtionals/index.js
@@ -13,7 +13,6 @@ console.log(x === y);     // false
 // always use === and !== over == and !=
 
 
-
 console.log(52 !== "52");    // true
 console.log(52 != "52");     // false
 console.log(!(52 === "52"));    // true
@@ -34,3 +33,42 @@ null
 NaN
 false
 
+
+if (x > 10) {
+  // if true
+} else if (x === 0) {
+  // 
+} else {
+  // otherwise here
+}
+
+if (x > 10) {
+  // if true
+} else {
+  // otherwise here
+}
+
+// ? :
+
+let carYear = 2023;
+let carKm = 105000;
+let currentYear = 2025;
+let warrenty;
+
+// Car has warrenty if less than 3 years and less than 100000 km
+if (currentYear - carYear <= 3 && carKm < 100000) {
+  warrenty = true;
+} else {
+  warrenty = false;
+}
+
+// (condition) ? <true return value> : <false return value>
+let warrenty2 = currentYear - carYear <= 3 && carKm < 100000 ? true : false;
+
+
+switch (x) {
+  case 1:
+  case 2:
+  case 3:
+  default:
+}
