49de373dc18e0f48b7162a80a3dc71a136804f1c
nirch <nirh2@yahoo.com>
2026-01-19 10:12:44 +0200

    commanf line arguments

---
+++ javascript/input/command-line-arguments.js
@@ -0,0 +1,8 @@
+
+// console.log(process.argv.length);
+console.log(process.argv);
+
+const myName = process.argv[2];
+const age = process.argv[3];
+
+console.log(`Hello ${myName}, you are ${age} years old`)
