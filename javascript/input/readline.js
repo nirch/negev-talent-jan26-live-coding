74bfe50726fa1b6b12e1b577518e9d316cf798c1
nirch <nirh2@yahoo.com>
2026-01-19 10:29:41 +0200

    readline

---
+++ javascript/input/readline.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+// readline is async it is not halting for the user input
+rl.question('What is your name? ', (nameAnswer) => {
+  rl.question('What is your age? ', (ageAnswer) => {
+    console.log(`Hello ${nameAnswer}, you are ${ageAnswer} years old`)
+    rl.close();
+  })
+});
+
+// this will print before the user input becuase readline is async
+console.log('eof');
