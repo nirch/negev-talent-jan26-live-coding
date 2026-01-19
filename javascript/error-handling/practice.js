0a67fa60b616526c883dc66785eb3a1b29116fe0
nirch <nirh2@yahoo.com>
2026-01-19 12:39:18 +0200

    try catch practice

---
+++ javascript/error-handling/practice.js
@@ -0,0 +1,50 @@
+
+try {
+  try {
+    throw new Error("oops");
+  } finally {
+    console.log("finally");
+  }
+} catch (ex) {
+  console.error("outer", ex.message);
+}
+
+// Output:
+// finally
+// outer oops
+
+try {
+  try {
+    throw new Error("oops");
+  } catch (ex) {
+    console.error("inner", ex.message);
+  } finally {
+    console.log("finally");
+  }
+} catch (ex) {
+  console.error("outer", ex.message);
+}
+
+// Output:
+
+// inner oops
+// finally
+
+try {
+  try {
+    throw new Error("oops");
+  } catch (ex) {
+    console.error("inner", ex.message);
+    throw ex;
+  } finally {
+    console.log("finally");
+  }
+} catch (ex) {
+  console.error("outer", ex.message);
+}
+
+// Output:
+
+// inner oops
+// finally
+// outer oops
