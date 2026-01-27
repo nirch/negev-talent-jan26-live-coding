6d0997ae82a4d60f514f94bfb13ae35cb1918a02
nirch <nirh2@yahoo.com>
2026-01-27 10:31:20 +0200

    feat: Implement a basic shopping list application with data model and UI rendering.

---
+++ js-and-web/data-to-ui/main.js
@@ -0,0 +1,33 @@
+import { getList, addItem } from './model.js'
+
+
+// UI
+function refreshShoppingList(shoppingList) {
+  const ul = document.querySelector('#shoppingList');
+
+  // 1) delete UI
+  ul.innerHTML = "";
+
+  // 2) Loop over shooping and add items 1 by 1
+  for (const item of shoppingList) {
+    const li = document.createElement('li');
+    li.textContent = item.name;
+    ul.appendChild(li);
+  }
+}
+
+// Controller
+addEventListener("DOMContentLoaded", () => {
+  refreshShoppingList(getList());
+
+
+  document.querySelector('#btnAdd').addEventListener("click", () => {
+    const input = document.querySelector('#inputNew')
+    const newItemValue = input.value;
+    // validate new item missing
+    addItem(newItemValue);
+    refreshShoppingList(getList());
+    input.value = "";
+  })
+
+});
---
+++ js-and-web/data-to-ui/model.js
@@ -0,0 +1,31 @@
+const shoppingList = [
+  {
+    id: "111",
+    name: "Milk",
+    picked: false
+  },
+  {
+    id: "222",
+    name: "Honey",
+    picked: false
+  },
+  {
+    id: "333",
+    name: "Bread",
+    picked: false
+  },
+]
+
+export function getList() {
+  const shoppingListClone = [...shoppingList];
+  return shoppingListClone;
+}
+
+export function addItem(itemName) {
+  shoppingList.push({
+    name: itemName,
+    picked: false
+  })
+}
+
+export function deleteItem(itemId) { }
